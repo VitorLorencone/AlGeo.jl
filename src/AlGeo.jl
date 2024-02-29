84133d4401672a95e889c35bbc44e13846333775
Vitor <vitorlorencone@gmail.com>
2024-02-29 00:32:15 -0300

    added some multi-threading testing

--- src/AlGeo.jl
+++ src/AlGeo.jl
@@ -5,7 +5,7 @@ TODO: Corrigir o show para mostrar o valor com subíndices
 module AlGeo
 
 export Algeo, id
-export Multivectors, AbstractGeometricAlgebraType, Blade, Multivector, OPTable, IPTable, GPTable ### Remover os 3 ultimos?
+export Multivectors, AbstractGeometricAlgebraType, Blade, Multivector, OPTable, IPTable, GPTable
 export bladeIndex, bladeScalar, bladeGeometricProduct, bladeInnerProduct, bladeOuterProduct, grade, gradeProjection, basisScalarProduct, multivectorSum, multivectorSub, multivectorOP, multivectorIP, multivectorGP
 
 include("Workspace.jl")
---
+++ src/ParallelOperations.jl
@@ -0,0 +1,41 @@
+include("Workspace.jl")
+using BenchmarkTools
+using Base.Threads
+
+mutable struct AtomicAbstractGeometricAlgebraType
+    valor::AbstractGeometricAlgebraType
+    lock::SpinLock
+end
+
+function initialize_atomic_agat(valor_inicial::AbstractGeometricAlgebraType)
+    return AtomicAbstractGeometricAlgebraType(valor_inicial, Threads.SpinLock())
+end
+
+function atomic_add_agat!(variavel::AtomicAbstractGeometricAlgebraType, valor::AbstractGeometricAlgebraType)
+    Threads.lock(variavel.lock)
+    variavel.valor = multivectorSum(variavel.valor, valor)
+    Threads.unlock(variavel.lock)
+end
+
+function multivectorGPMulti(ei::AbstractGeometricAlgebraType, ej::AbstractGeometricAlgebraType)::AbstractGeometricAlgebraType
+    result = initialize_atomic_agat(Multivectors([1],[0]))
+    Threads.@threads for i in eachindex(ei.val.nzind)
+        Threads.@threads for j in eachindex(ej.val.nzind)
+            atomic_add_agat!(result, bladeGeometricProduct(Multivectors([ei.val.nzind[i]],[ei.val.nzval[i]]), Multivectors([ej.val.nzind[j]],[ej.val.nzval[j]])))
+        end
+    end
+    return result.valor
+end
+
+n = 2
+Algeo(n)
+mv = Multivectors(collect(1:2^n), ones(2^n))
+
+println(Threads.nthreads())
+
+result_sem_threads = @benchmark  multivectorGP(mv, mv)
+result_com_threads = @benchmark  multivectorGPMulti(mv, mv)
+
+show(stdout, "text/plain", result_sem_threads)
+println()
+show(stdout, "text/plain", result_com_threads)
